f332b3f80cc0a6f31876a9fa418303f7b84f85d7
Kelli Parris <kelli@hsdesignhouse.com>
2017-01-27 12:39:27 -0600

    home content

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -5,4 +5,24 @@ class LinksController < ApplicationController
 	def pink
 	    redirect_to('http://www.pinkfiretrucks.org/')
 	end
+
+	def ginnylane
+	    redirect_to('ginnylanebargrill.com')
+	end
+
+	def villagio
+	    redirect_to('villaggiogrille.com')
+	end
+
+	def hotspot
+	    redirect_to('http://www.thehotspotoba.com/')
+	end
+
+	def wharf
+	    redirect_to('http://www.alwharf.com/')
+	end
+
+	def fb
+		redirect_to('https://www.facebook.com/jason.ramirez.796?fref=ts')
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
   get '/pink', to: 'links#pink'
+  get '/ginnylane', to: 'links#ginnylane'
+  get '/villaggio', to: 'links#villaggio'
+  get '/wharf', to: 'links#wharf'
+  get '/hotspot', to: 'links#hotspot'
+  get '/fb', to: 'links#fb'
+
   # resources :registration, only: [:new, :create]
 
   get 'registration', to: 'registration#new', as: 'registration'
